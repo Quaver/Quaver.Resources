5b19a28b312a52f4ace605966ac1a66d6e48a13a
Swan <me@swan.moe>
2018-05-16 18:42:42 -0400

    Add Assistant font 16pt

--- Quaver.Resources/QuaverResources.Designer.cs
+++ Quaver.Resources/QuaverResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Quaver.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class QuaverResources {
@@ -1757,6 +1757,26 @@ namespace Quaver.Resources {
             get {
                 object obj = ResourceManager.GetObject("arrow_stage_timingbar", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] assistant_light_16 {
+            get {
+                object obj = ResourceManager.GetObject("assistant_light_16", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Byte[].
+        /// </summary>
+        public static byte[] assistant_regular_16 {
+            get {
+                object obj = ResourceManager.GetObject("assistant_regular_16", resourceCulture);
+                return ((byte[])(obj));
             }
         }
         
