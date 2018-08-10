bee7dc87a206fee2f14ac76362e5541bd6551568
Swan <me@swan.moe>
2018-08-10 16:15:17 -0400

    Add select-border

--- Quaver.Resources/QuaverResources.Designer.cs
+++ Quaver.Resources/QuaverResources.Designer.cs
@@ -7003,6 +7003,16 @@ namespace Quaver.Resources {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        public static System.Drawing.Bitmap select_border {
+            get {
+                object obj = ResourceManager.GetObject("select_border", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         public static System.Drawing.Bitmap set_select_mask {
             get {
                 object obj = ResourceManager.GetObject("set_select_mask", resourceCulture);
